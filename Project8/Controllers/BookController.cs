76206625a6f00e1316984b1fd65427d284446910
Andyy <Andyy@DUY-PC>
2025-11-24 02:34:06 +0700

    Cải thiện UI

--- Project8/Controllers/BookController.cs
+++ Project8/Controllers/BookController.cs
@@ -49,19 +49,27 @@ namespace WebBanSach.Controllers
 
         //GET : /Book/RelatedBooks : hiển thị sách liên quan ở trang chi tiết sản phẩm
         //Partial View : RelatedBooks
-        public ActionResult RelatedBooks(int id, int count = 3)
+        [ChildActionOnly]
+        public ActionResult RelatedBooks(int? id, int count = 3)
         {
-            var book = db.Saches.Find(id);
-            if (book == null)
-                return PartialView(new List<Sach>());
+            if (!id.HasValue)
+                return new EmptyResult();   // không có id thì không render gì, tránh 500
 
-            var related = db.Saches.Where(s => s.MaSach != id &&
-                (s.MaLoai == book.MaLoai || s.MaTG == book.MaTG || s.MaNXB == book.MaNXB))
-                .OrderByDescending(s => s.NgayCapNhat)
-                .Take(count)
-                .ToList();
+            var book = db.Saches.Find(id.Value);
+            if (book == null)
+                return new EmptyResult();
+
+            var related = db.Saches.Where(s => s.MaSach != id.Value &&
+                                               (s.MaLoai == book.MaLoai ||
+                                                s.MaTG == book.MaTG ||
+                                                s.MaNXB == book.MaNXB))
+                                    .OrderByDescending(s => s.NgayCapNhat)
+                                    .Take(count)
+                                    .ToList();
+
             return PartialView("RelatedBooks", related);
         }
+
 
         //GET : /Book/DidYouSee : hiển thị ra 3 cuốn sách giảm dần theo ngày
         //Parital View : DidYouSee
@@ -73,18 +81,98 @@ namespace WebBanSach.Controllers
         }
 
         //GET : /Book/All : hiển thị tất cả sách trong db
-        public ActionResult ShowAllBook(int? page)
+        //GET : /Book/All : hiển thị tất cả sách trong db + lọc & sắp xếp
+        public ActionResult ShowAllBook(
+    int? page,
+    int? categoryId,
+    int? publisherId,
+    int? authorId,
+    string priceRange,
+    string sortOrder)
         {
-            //tạo biến số sản phẩm trên trang
-            int pageSize = 10;
+            int pageSize = 12;
+            int pageNumber = page ?? 1;
 
-            //tạo biến số trang
-            int pageNumber = (page ?? 1);
+            var query = db.Saches.AsQueryable();
 
-            var result = new BookProcess().ShowAllBook().ToPagedList(pageNumber, pageSize);
+            // Lọc theo thể loại
+            if (categoryId.HasValue)
+            {
+                query = query.Where(s => s.MaLoai == categoryId.Value);
+            }
+
+            // Lọc theo NXB
+            if (publisherId.HasValue)
+            {
+                query = query.Where(s => s.MaNXB == publisherId.Value);
+            }
+
+            // Lọc theo tác giả
+            if (authorId.HasValue)
+            {
+                query = query.Where(s => s.MaTG == authorId.Value);
+            }
+
+            // Lọc theo khoảng giá
+            if (!string.IsNullOrEmpty(priceRange))
+            {
+                switch (priceRange)
+                {
+                    case "1": // dưới 50k
+                        query = query.Where(s => s.GiaBan.HasValue && s.GiaBan.Value < 50000);
+                        break;
+                    case "2": // 50k - 100k
+                        query = query.Where(s => s.GiaBan.HasValue && s.GiaBan.Value >= 50000 && s.GiaBan.Value <= 100000);
+                        break;
+                    case "3": // 100k - 200k
+                        query = query.Where(s => s.GiaBan.HasValue && s.GiaBan.Value >= 100000 && s.GiaBan.Value <= 200000);
+                        break;
+                    case "4": // 200k - 500k
+                        query = query.Where(s => s.GiaBan.HasValue && s.GiaBan.Value >= 200000 && s.GiaBan.Value <= 500000);
+                        break;
+                    case "5": // trên 500k
+                        query = query.Where(s => s.GiaBan.HasValue && s.GiaBan.Value > 500000);
+                        break;
+                }
+            }
+
+            // Sắp xếp
+            switch (sortOrder)
+            {
+                case "price_asc":
+                    query = query.OrderBy(s => s.GiaBan);
+                    break;
+                case "price_desc":
+                    query = query.OrderByDescending(s => s.GiaBan);
+                    break;
+                case "date_desc":
+                    query = query.OrderByDescending(s => s.NgayCapNhat);
+                    break;
+                default:
+                    query = query.OrderBy(s => s.TenSach);
+                    break;
+            }
+
+            // Dropdown lấy từ DB (TheLoai, NXB, TacGia)
+            ViewBag.CategoryList  = new SelectList(db.TheLoais.OrderBy(t => t.TenLoai), "MaLoai", "TenLoai");
+ViewBag.PublisherList = new SelectList(db.NhaXuatBans.OrderBy(n => n.TenNXB), "MaNXB", "TenNXB");
+ViewBag.AuthorList    = new SelectList(db.TacGias.OrderBy(a => a.TenTG), "MaTG", "TenTG");
+
+
+            // Giữ lại giá trị filter
+            ViewBag.CategoryId = categoryId;
+            ViewBag.PublisherId = publisherId;
+            ViewBag.AuthorId = authorId;
+            ViewBag.PriceRange = priceRange;
+            ViewBag.SortOrder = sortOrder;
+
+            var result = query.ToPagedList(pageNumber, pageSize);
 
             return View(result);
         }
+
+
+
 
     }
 }
